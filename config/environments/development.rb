093aba0f5c0a8bc60f03cb2dca4f8beae2d33ab9
Kris Birov <kris.birov@gmail.com>
2019-12-16 13:49:32 +0200

    development.rb 1

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -10,7 +10,7 @@ Rails.application.configure do
   config.eager_load = false
 
   # Show full error reports.
-  config.consider_all_requests_local = true
+  # This line is changed
 
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
